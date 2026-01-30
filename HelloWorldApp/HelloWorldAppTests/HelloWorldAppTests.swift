725b5839f5f060a5ddb523d0a056f670afdf53c7
rthgct <rthgct@umsystem.edu>
2026-01-30 12:16:27 -0600

    Made project & took screenshot

---
+++ HelloWorldApp/HelloWorldAppTests/HelloWorldAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldAppTests.swift
+//  HelloWorldAppTests
+//
+//  Created by user287316 on 1/30/26.
+//
+
+import Testing
+@testable import HelloWorldApp
+
+struct HelloWorldAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
